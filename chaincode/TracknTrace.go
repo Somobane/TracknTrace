7e93042edbeabf2bd7f99424cc447ce3fad809cf
Your Name <you@example.com>
2017-05-07 11:00:17 +0530

    API for Package Line added: 7th May, 2017

--- chaincode/TracknTrace.go
+++ chaincode/TracknTrace.go
@@ -103,14 +103,14 @@ func (t *TnT) Init(stub shim.ChaincodeStubInterface, function string, args []str
 
 
 	// Check if table already exists: Packaging Line
-	_, err = stub.GetTable("PackagingLine")
+	_, err = stub.GetTable("PackageLine")
 	if err == nil {
 		// Table already exists; do not recreate
 		return nil, nil
 	}
 
 	// Create application Table
-	err = stub.CreateTable("PackagingLine", []*shim.ColumnDefinition{
+	err = stub.CreateTable("PackageLine", []*shim.ColumnDefinition{
 		&shim.ColumnDefinition{Name: "caseId", Type: shim.ColumnDefinition_STRING, Key: true},
 		&shim.ColumnDefinition{Name: "holderAssemblyId", Type: shim.ColumnDefinition_STRING, Key: false},
 		&shim.ColumnDefinition{Name: "chargerAssemblyId", Type: shim.ColumnDefinition_STRING, Key: false},
@@ -192,11 +192,11 @@ if len(args) != 11 {
 
 }
 
-//Update AssemblyLine status
+//Update Assembly based on Id
 func (t *TnT) updateAssemblyByID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
 	if len(args) != 14 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 12.")
+		return nil, errors.New("Incorrect number of arguments. Expecting 14.")
 	} 
 	
 		_assemblyId := args[0]
@@ -211,12 +211,10 @@ func (t *TnT) updateAssemblyByID(stub shim.ChaincodeStubInterface, args []string
 		_StickPodBatchId:=args[9]
 		_ManufacturingPlant:=args[10]
 		_AssemblyStatus:= args[11]
-
-		_time:= time.Now().Local()
-
 		_AssemblyCreationDate := args[12]
-		_AssemblyLastUpdateOn := _time.Format("2006-01-02")
 		_AssemblyCreatedBy :=  args[13]
+		_time:= time.Now().Local()
+		_AssemblyLastUpdateOn := _time.Format("2006-01-02")
 		_AssemblyLastUpdatedBy := ""
 
 
@@ -255,10 +253,114 @@ func (t *TnT) updateAssemblyByID(stub shim.ChaincodeStubInterface, args []string
 			}})
 
 		if error_ != nil {
-			return nil, err 
+			return nil, error_ 
 		}
 		if !ok && error_ == nil {
 			return nil, errors.New("Row already exists in Assemblyline.")
+		}
+		
+	return nil, nil
+
+}
+
+//API to create a package
+func (t *TnT) createPackage(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+		if len(args) != 5 {
+			return nil, fmt.Errorf("Incorrect number of arguments. Expecting 5. Got: %d.", len(args))
+		}
+	
+		rand.Seed(time.Now().Unix())
+		
+		_caseId := strconv.Itoa(rand.Int())
+		_holderAssemblyId:= args[0]
+		_chargerAssemblyId:=args[1]
+		_packageStatus:=args[2]
+		_packagingDate:=args[3]
+		_shippingtoAddress:=args[4]
+		_time:= time.Now().Local()
+
+		_packageCreationDate := _time.Format("2006-01-02")
+		_packageLastUpdateOn := _time.Format("2006-01-02")
+		_packageCreatedBy := ""
+		_packageLastUpdatedBy := ""
+
+		// Insert a row
+		ok, err := stub.InsertRow("PackageLine", shim.Row{
+			Columns: []*shim.Column{
+				&shim.Column{Value: &shim.Column_String_{String_: _caseId}},
+				&shim.Column{Value: &shim.Column_String_{String_: _holderAssemblyId}},
+				&shim.Column{Value: &shim.Column_String_{String_: _chargerAssemblyId}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageStatus}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packagingDate}},
+				&shim.Column{Value: &shim.Column_String_{String_: _shippingtoAddress}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageCreationDate}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageLastUpdateOn}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageCreatedBy}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageLastUpdatedBy}},	
+		}})
+		if err != nil {
+			return nil, err 
+		}
+		if !ok && err == nil {
+			return nil, errors.New("Row already exists.")
+		}
+
+		//Update the holder and charger assembly id status as "Packaged" - implement later
+		return nil, nil
+
+}
+
+//Update Package based on CaseId
+func (t *TnT) updatePackageByCaseID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 8 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 14.")
+		} 
+	
+		_caseId := args[0]
+		_holderAssemblyId:= args[1]
+		_chargerAssemblyId:=args[2]
+		_packageStatus:=args[3]
+		_packagingDate:=args[4]
+		_shippingtoAddress:=args[5]
+		_packageCreationDate := args[6]
+		_packageCreatedBy :=  args[7]
+		_time:= time.Now().Local()
+
+		_packageLastUpdateOn := _time.Format("2006-01-02")
+		_packageLastUpdatedBy := ""
+
+		// Get the row pertaining to this Assembly Id
+		var columns []shim.Column
+		col1 := shim.Column{Value: &shim.Column_String_{String_: _caseId}}
+		columns = append(columns, col1)
+		// Delete the row pertaining to this assemblyId
+		err := stub.DeleteRow(
+			"PackageLine",
+			columns,
+		)
+		if err != nil {
+			return nil, errors.New("Failed deleting row.")
+		}
+
+		// Insert a row
+		ok, _error := stub.InsertRow("PackageLine", shim.Row{
+			Columns: []*shim.Column{
+				&shim.Column{Value: &shim.Column_String_{String_: _caseId}},
+				&shim.Column{Value: &shim.Column_String_{String_: _holderAssemblyId}},
+				&shim.Column{Value: &shim.Column_String_{String_: _chargerAssemblyId}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageStatus}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packagingDate}},
+				&shim.Column{Value: &shim.Column_String_{String_: _shippingtoAddress}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageCreationDate}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageLastUpdateOn}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageCreatedBy}},
+				&shim.Column{Value: &shim.Column_String_{String_: _packageLastUpdatedBy}},
+			}})
+		if _error != nil {
+			return nil, _error 
+		}
+		if !ok && _error == nil {
+			return nil, errors.New("Row already exists.")
 		}
 		
 	return nil, nil
@@ -312,7 +414,7 @@ var columns []shim.Column
 func (t *TnT) getAssemblyByID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
 	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting AssemblyLineID to query")
+		return nil, errors.New("Incorrect number of arguments. Expecting AssemblyID to query")
 	}
 
 	_assemblyId := args[0]
@@ -394,6 +496,75 @@ func (t *TnT) getAllAssemblyByStatus(stub shim.ChaincodeStubInterface, args []st
 
 }
 
+//get all Packages
+func (t *TnT) getAllPackage(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {	
+	var columns []shim.Column
+
+	rows, err := stub.GetRows("PackageLine", columns)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to retrieve row")
+	}
+	
+	res2E:= []*PackageLine{}	
+	
+	for row := range rows {		
+		newApp:= new(PackageLine)
+		newApp.CaseId = row.Columns[0].GetString_()
+		newApp.HolderAssemblyId = row.Columns[1].GetString_()
+		newApp.ChargerAssemblyId = row.Columns[2].GetString_()
+		newApp.PackageStatus = row.Columns[3].GetString_()
+		newApp.PackagingDate = row.Columns[4].GetString_()
+		newApp.ShippingToAddress = row.Columns[5].GetString_()
+		newApp.PackageCreationDate = row.Columns[6].GetString_()
+		newApp.PackageLastUpdatedOn = row.Columns[7].GetString_()
+		newApp.PackageCreatedBy = row.Columns[8].GetString_()
+		newApp.PackageLastUpdatedBy  = row.Columns[9].GetString_()
+		if len(newApp.CaseId) > 0{
+		res2E=append(res2E,newApp)		
+		}				
+	}
+	
+    mapB, _ := json.Marshal(res2E)
+    fmt.Println(string(mapB))
+	
+	return mapB, nil
+
+}
+
+//get the Package against ID
+func (t *TnT) getPackageByID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting Case Id to query")
+	}
+
+	_caseId := args[0]
+	
+
+	// Get the row pertaining to this assemblyID
+	var columns []shim.Column
+	col1 := shim.Column{Value: &shim.Column_String_{String_: _caseId}}
+	columns = append(columns, col1)
+
+	row, err := stub.GetRow("PackageLine", columns)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get the data for the assembly " + _caseId + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	// GetRows returns empty message if key does not exist
+	if len(row.Columns) == 0 {
+		jsonResp := "{\"Error\":\"Failed to get the data for the assembly " + _caseId + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	//return []byte (row), nil
+	 mapB, _ := json.Marshal(row)
+    fmt.Println(string(mapB))
+	
+	return mapB, nil
+
+}
 
 // Invoke callback representing the invocation of a chaincode
 func (t *TnT) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
@@ -409,7 +580,13 @@ func (t *TnT) Invoke(stub shim.ChaincodeStubInterface, function string, args []s
 	} else if function == "updateAssemblyByID" {
 		fmt.Printf("Function is updateAssemblyByID")
 		return t.updateAssemblyByID(stub, args)
-	} 
+	}else if function == "createPackage" {
+		fmt.Printf("Function is createPackage")
+		return t.createPackage(stub, args)
+	} else if function == "updatePackageByCaseID" {
+		fmt.Printf("Function is updatePackageByCaseID")
+		return t.updatePackageByCaseID(stub, args)
+	}  
 
 	return nil, errors.New("Received unknown function invocation")
 }
@@ -429,6 +606,12 @@ func (t *TnT) Query(stub shim.ChaincodeStubInterface, function string, args []st
 	}else if function == "getAllAssembly" { 
 		t := TnT{}
 		return t.getAllAssembly(stub, args)
+	}else if function == "getAllPackage" { 
+		t := TnT{}
+		return t.getAllPackage(stub, args)
+	}else if function == "getPackageByID" { 
+		t := TnT{}
+		return t.getPackageByID(stub, args)
 	}
 	
 	return nil, errors.New("Received unknown function query")
